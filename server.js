c29738d162a2739a35fbff2106a0373ae340512e
sifracapitalgroup <sifra.capitalgroup@gmail.com>
2026-05-14 16:38:59 -0400

    Implementing Elevenlabs voice -> converting OPEN AI AUDIO -> TEXT

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const http = require("http");
 const WebSocket = require("ws");
 const twilio = require("twilio");
-
+///
 // 👉 ADD THIS HERE
 const twilioClient = twilio(
   process.env.TWILIO_ACCOUNT_SID,
